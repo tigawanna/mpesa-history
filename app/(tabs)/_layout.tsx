56fa245fa3054ce4189193d7be14b7db16d65580
tigawanna <denniskinuthiawaweru@gmail.com>
2025-04-15 19:47:18 +0300

    Refactor history management components and enhance UI with new settings tab. Update color constants for improved theming. Remove deprecated history components.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,61 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol, MaterialIcon } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { useTheme } from "react-native-paper";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  const { colors } = useTheme();
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        // tabBarActiveTintColor: Colors[colorScheme ?? "light"].primary,
+
+        headerStyle: {
+          backgroundColor: colors.surface,
+        },
+
+        // headerBackButtonDisplayMode: "generic",
+
+        // tabBarInactiveTintColor: Colors[colorScheme ?? "light"].primary,
+        // headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+            position: "absolute",
           },
-          default: {},
+          default: {
+            backgroundColor: colors.surface,
+          },
         }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: "Explore",
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }) => <MaterialIcon size={28} name="settings" color={color} />,
         }}
       />
     </Tabs>
--- components/history/HistoryList.tsx
+++ components/history/HistoryList.tsx
@@ -1,114 +1,19 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { Avatar, Checkbox, Chip, List, Searchbar, Surface, useTheme } from "react-native-paper";
-// import { useLiveQuery } from "drizzle-orm/expo-sqlite";
-// import { useDrizzle } from "@/db/useDrizzle";
-// import { formatDate } from "@/utils/date";
-// import { getHistory } from "@/db/data/history";
-import { HistoryDelete } from "./HistoryDelete";
-import { HistoryFormCreate } from "./HistoryFormCreate";
-import { HistoryFormUpdate } from "./HistoryFormUpdate";
-import {setStringAsync} from "expo-clipboard";
-// import { InsertHistoryEntry } from "@/db/validators";
-
-// TODO handle large lists with load more and add indexes to name and number
-
-export function HistoryList(): JSX.Element {
-  // const db = useDrizzle();
-  // const [searchQuery, setSearchQuery] = React.useState("");
-  // const [reaload, setReload] = React.useState(0);
-  // const { data, error } = useLiveQuery(getHistory(db,searchQuery), [searchQuery, reaload]);
-
-  const [selected, setSelected] = React.useState<number[] | null>(null);
-
-  const handlelongPress = (id: number) => {
-    setSelected([id]);
-  };
-  const handleAddOrRemoveOnPress = (id: number) => {
-    if (!selected || selected?.length === 0) return;
-    setSelected((prev) => {
-      if (!prev) return [id];
-      if (prev.includes(id)) {
-        return prev.filter((item) => item !== id);
-      }
-      return [...prev, id];
-    });
-  };
-  const handlePressOnce = (item: InsertHistoryEntry) => {
-        if (!selected || selected?.length === 0) {
-         setStringAsync(item.number);
-        }else{
-          if(!item.id) return
-          handleAddOrRemoveOnPress(item.id);
-        }
-  };
-
-  const { colors } = useTheme();
-  const isAllSelecetd = selected?.length === data.length;
-  const isSelecting = selected && selected?.length > 0;
-  return (
-    <Surface style={styles.container}>
-      <Surface style={styles.controlsContaner}>
-        <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
-        {isSelecting && (
-          <Surface style={{ flexDirection: "row", justifyContent: "space-between", padding: 8 }}>
-            <Chip>{selected?.length} Selected</Chip>
-            <HistoryDelete selected={selected} setReload={setReload} setSelected={setSelected} />
-            <Checkbox
-              status={isAllSelecetd ? "checked" : "unchecked"}
-              onPress={() => {
-                if (isAllSelecetd) {
-                  setSelected([]);
-                } else {
-                  setSelected(data.map((item) => item.id));
-                }
-              }}
-            />
-          </Surface>
-        )}
-      </Surface>
-      <List.Section style={styles.listSection}>
-        {data.map((transaction) => {
-          const isItemSelected = selected?.includes(transaction.id);
-          return (
-            <List.Item
-              onLongPress={() => handlelongPress(transaction.id)}
-              onPress={() => handlePressOnce(transaction)}
-              rippleColor={colors.primary}
-              key={transaction.id}
-              title={transaction.name}
-              style={{
-                ...styles.listItem,
-                backgroundColor: isItemSelected ? colors.inversePrimary : colors.elevation.level4,
-              }}
-              description={`${transaction.number} ${formatDate(transaction.createdAt)}`}
-              left={(props) => <Avatar.Text size={40} label={transaction.name?.[0]} />}
-              right={(props) => <HistoryFormUpdate item={transaction} />}
-            />
-          );
-        })}
-      </List.Section>
-      <HistoryFormCreate />
-    </Surface>
-  );
+import { StyleSheet } from 'react-native'
+import { Text,Surface } from 'react-native-paper';
+ 
+export function HistoryList(){
+return (
+<Surface style={{ ...styles.container }}>
+    <Text variant='titleLarge'>HistoryList</Text>
+</Surface>
+);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    elevation: 1,
-    paddingHorizontal: 4,
-  },
-  controlsContaner: {
-    elevation: 1,
-  },
-  listSection: {
-    paddingVertical: 8,
-    gap: 4,
-  },
-  listItem: {
-    paddingVertical: 8,
-    paddingHorizontal: 4,
-    borderRadius: 8,
-  },
-});
+container:{
+  flex:1,
+  height:'100%',
+   width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+})
